a87f74dbee41388f83c3f80bbf1ee797632359d6
MustafaGuer <mustafaguer@outlook.com>
2023-01-31 17:25:35 +0100

    first fin: header && header button

---
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.jsx
@@ -0,0 +1,17 @@
+import CartIcon from "../../Cart/CartIcon";
+
+import styles from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={styles.button}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span className={styles.badge}>Your Cart</span>
+      <span>3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
