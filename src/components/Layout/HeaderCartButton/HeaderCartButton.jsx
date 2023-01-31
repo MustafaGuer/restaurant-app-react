95a4ee19af93a4c46c253b8fcddc48b2b3fbdb94
MustafaGuer <mustafaguer@outlook.com>
2023-01-31 21:06:17 +0100

    add: bump effect header btn

--- src/components/Layout/HeaderCartButton/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "../../Cart/CartIcon";
 import CartContext from "../../../store/cart-context";
@@ -6,14 +6,34 @@ import CartContext from "../../../store/cart-context";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNum, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNum, item) => {
     return curNum + item.amount;
   }, 0);
 
+  const btnStyles = `${styles.button} ${btnIsHighlighted ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onClick} className={styles.button}>
+    <button onClick={props.onClick} className={btnStyles}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
