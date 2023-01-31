5a734b07ff5f833a9f62689d27f080ec62624e9f
MustafaGuer <mustafaguer@outlook.com>
2023-01-31 18:10:38 +0100

    little fix: id issue of item form

--- src/components/Meals/AvailableMeals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals/AvailableMeals.jsx
@@ -6,7 +6,15 @@ import { DUMMY_MEALS } from "../dummy-meals";
 import MealItem from "../MealItem/MealItem";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+      id={meal.id}
+    />
+  ));
 
   return (
     <section className={styles.meals}>
